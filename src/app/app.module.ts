d039b203ce3837a8d0ed8127ecd57ee2d4ae2002
vickypaul <vickypaul123@gmail.com>
2022-11-26 23:56:37 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StockComponent } from './views/stock/stock.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+  ],
   declarations: [AppComponent, HelloComponent, StockComponent],
   bootstrap: [AppComponent],
 })
