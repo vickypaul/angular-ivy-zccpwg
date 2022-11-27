e86a953d8b587610a46f35b2335da2710be5c4b9
vickypaul <vickypaul123@gmail.com>
2022-11-27 10:14:27 +0530

    Added back to stock functionality

--- src/app/views/sentiment/sentiment.component.ts
+++ src/app/views/sentiment/sentiment.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sentiment',
   templateUrl: './sentiment.component.html',
-  styleUrls: ['./sentiment.component.css']
+  styleUrls: ['./sentiment.component.css'],
 })
 export class SentimentComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goBacktoStock() {
+    this.router.navigate(['']);
   }
-
 }
