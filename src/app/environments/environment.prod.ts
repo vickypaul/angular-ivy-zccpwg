3738cbe168a5f8d1a5bbdf96eb921a5f006212d2
vickypaul <vickypaul123@gmail.com>
2022-11-28 23:55:56 +0530

    1 file modified

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  envName: 'dev',
+  envName: 'prod',
   QUOTE_ENDPOINT: 'https://finnhub.io/api/v1/quote',
   SYMBOL_ENDPOINT: 'https://finnhub.io/api/v1/search',
   API_KEY: 'bu4f8kn48v6uehqi3cqg',
